0d18f1122de901f7b24929ea728df37c674ad9ef
suhail <suhailkv99@gmail.com>
2022-06-08 18:31:19 +0530

    changed express server to http server

--- app.js
+++ app.js
@@ -1,49 +1,57 @@
-const express = require("express");
-const app = express();
 const https = require("https");
+const http = require("http");
 const url = "https://time.com/";
 
-app.get("/getTimeStories", (req, resp) => {
-  https
-    .get(url, (res) => {
-      res.setEncoding("utf8");
+var server = http.createServer(function (req, resp) {
+  // 2 - creating server
 
-      let response = "";
-      res.on("data", (data) => {
-        response += data;
+  if (req.url == "/getTimeStories") {
+    https
+      .get(url, (res) => {
+        res.setEncoding("utf8");
+
+        let response = "";
+        res.on("data", (data) => {
+          response += data;
+        });
+
+        res.on("end", function jsonData() {
+          let latestStories = [];
+          const storyExtractor = response.match(
+            /<h3 class="latest-stories__item-headline">(.*?)</g
+          );
+          if (storyExtractor != null) {
+            storyExtractor.forEach((content) => {
+              latestStories.push({ title: content.split(">")[1].slice(0, -1) });
+            });
+          }
+
+          let links = [];
+          const linkExtractor = response.match(
+            /latest-stories__item">\n(.*)">/g
+          );
+          if (linkExtractor != null) {
+            linkExtractor.forEach((content) => {
+              links.push(url + content.split('"')[2]);
+            });
+          }
+
+          const newData = latestStories.map((story, link) => ({
+            ...story,
+            link: links[link],
+          }));
+
+          resp.write(JSON.stringify(newData));
+          resp.end();
+        });
+      })
+      .on("error", function (err) {
+        console.log(err);
       });
-
-      res.on("end", function jsonData() {
-        let latestStories = [];
-        const storyExtractor = response.match(
-          /<h3 class="latest-stories__item-headline">(.*?)</g
-        );
-        if (storyExtractor != null) {
-          storyExtractor.forEach((content) => {
-            latestStories.push({ title: content.split(">")[1].slice(0, -1) });
-          });
-        }
-
-        let links = [];
-        const linkExtractor = response.match(/latest-stories__item">\n(.*)">/g);
-        if (linkExtractor != null) {
-          linkExtractor.forEach((content) => {
-            links.push(url + content.split('"')[2]);
-          });
-        }
-
-        const newData = latestStories.map((story, link) => ({
-          ...story,
-          link: links[link],
-        }));
-        resp.json(newData);
-      });
-    })
-    .on("error", function (err) {
-      console.log(err);
-    });
+  } else {
+    resp.write(JSON.stringify({ message: "Error occured" }));
+    resp.end();
+  }
 });
 
-app.listen(3000, () => {
-  console.log("server working perfectly");
-});
+server.listen(3000);
